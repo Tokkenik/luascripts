28b20bef27ef03e05a9be94e779053c324017c36
Tokkenik <86359449+Tokkenik@users.noreply.github.com>
2023-04-05 20:05:41 +0200

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -23,42 +23,35 @@ local Functions = {
     Pathfinding = function(name, targetPosition)
         if not getgenv().pathfindingrunning then
             getgenv().pathfindingrunning = true
-            local Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid")
-            local Body = game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-            local path = game:GetService("PathfindingService"):CreatePath({
-                AgentRadius = 1.5,
-                AgentHeight = 6,
-            	AgentCanJump = true,
-            	AgentCanClimb = true,
-            	WaypointSpacing = 3,
-            })
-            path:ComputeAsync(Body.Position, targetPosition)
-            if path.Status == Enum.PathStatus.Success then
-                local wayPoints = path:GetWaypoints()
-                elapsedTime = 0
-                for i = 1, #wayPoints do
-                    local point = wayPoints[i]
-                    getgenv().pathfindingrunning = true
-                    repeat
-                        Humanoid:MoveTo(point.Position)
-                        elapsedTime += task.wait()
-                        if Humanoid.MoveToFinished:Wait() then
-                            break
-                        end
-                    until elapsedTime > 3
-                    if not Humanoid.MoveToFinished:Wait() then
-                        Humanoid.Jump = true
-                        print("Retrying...")
-                        repeat
-                            Humanoid:MoveTo(point.Position)
-                            elapsedTime += task.wait()
-                        until elapsedTime > 3
-                    end
-                    getgenv().pathfindingrunning = false
-                end
+            local PFS = game:GetService("PathfindingService")
+            local path = PFS:CreatePath()
+            local test = game:GetService("Workspace"):WaitForChild(game:GetService("Players").LocalPlayer.Name)
+            local hum = test:WaitForChild("Humanoid")
+            local HRP = test:WaitForChild("HumanoidRootPart")
+            local success, errorMsg = pcall(function()
+            	path:ComputeAsync(HRP.Position, targetPosition)
+            end)
+            if success and path.Status == Enum.PathStatus.Success then
+            	local waypoints = path:GetWaypoints()
+            	for _, waypoint in pairs(waypoints) do
+            		if waypoint.Action == Enum.PathWaypointAction.Jump and hum.Jump == false then
+            			hum.Jump = true
+            			wait()
+            			hum.Jump = false
+            		end
+            		repeat
+            		    hum:MoveTo(waypoint.Position)
+            		until HRP.Position == waypoint.Position
+            		if HRP.Position == targetPosition then
+            			break
+            		end
+            	end
+            elseif path.Status == Enum.PathStatus.NoPath then
+            	print("No possible path")
+            else
+            	warn("Failed to compute path: ", errorMsg)
             end
             getgenv().pathfindingrunning = false
-        else
         end
     end
 }
